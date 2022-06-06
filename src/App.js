d70566ad8b8b9b5a10b4ca13b98bddeb86fc7b70
Zhiping Yu <yuzh@hhsc.ca>
2022-06-06 10:52:00 -0400

    #1: completed heading and subheading

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedOption: ""
+    };
+  }
+
+  handleOptionChange = changeEvent => {
+    this.setState({
+      selectedOption: changeEvent.target.value
+    });
+  };
+
+  handleFormSubmit = formSubmitEvent => {
+    formSubmitEvent.preventDefault();
+    console.log("You have submitted:", this.state.selectedOption);
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row mt-5">
+          <div className="col-sm-12">
+          <div id="heading">
+            <h1>PELVIC FLOOR IMPACT QUESTIONNAIRE</h1>
+            <h2>
+                How do the following symptoms or conditions usually affect each of the three ereas below?
+            </h2>
+          </div>
+            <form onSubmit={this.handleFormSubmit}>
+              <div className="form-check">
+                <label>
+                  <input
+                    type="radio"
+                    name="react-tips"
+                    value="option1"
+                    checked={this.state.selectedOption === "option1"}
+                    onChange={this.handleOptionChange}
+                    className="form-check-input"
+                  />
+                  Option 1
+                </label>
+              </div>
+              <div className="form-check">
+                <label>
+                  <input
+                    type="radio"
+                    name="react-tips"
+                    value="option2"
+                    checked={this.state.selectedOption === "option2"}
+                    onChange={this.handleOptionChange}
+                    className="form-check-input"
+                  />
+                  Option 2
+                </label>
+              </div>
+              <div className="form-check">
+                <label>
+                  <input
+                    type="radio"
+                    name="react-tips"
+                    value="option3"
+                    checked={this.state.selectedOption === "option3"}
+                    onChange={this.handleOptionChange}
+                    className="form-check-input"
+                  />
+                  Option 3
+                </label>
+              </div>
+              <div className="form-group">
+                <button className="btn btn-primary mt-2" type="submit">
+                  Save
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
